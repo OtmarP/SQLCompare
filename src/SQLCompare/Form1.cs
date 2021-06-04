8f00c57e476c10f34f943379765d3bb0ebe89692
OtmarP (opMedion14b) <Otmar.Pilgerstorfer@chello.at>
2021-06-04 14:59:47 +0200

    Form1_Load #1

--- src/SQLCompare/Form1.cs
+++ src/SQLCompare/Form1.cs
@@ -12,9 +12,27 @@ namespace SQLCompare
 {
     public partial class Form1 : Form
     {
+        private string _project_compair;    // XXX_Kunde
+        private string _project_project;    // XXX
+        private string _version;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Version
+            //-----------------------------------------------------------------------
+            // Mi.01.04.2014 18:20:00 -op- Cr.
+            //-----------------------------------------------------------------------
+            _version = "20161205_125738_Mo";
+            // Old: "20160720_173725_Mi"
+        }
+
+        private void SetContextMenue()
+        {
         }
     }
 }
