6bf469843ec681a815e04203d6de4975281ec786
Otmar Pilgerstorfer <Otmar.Pilgerstorfer@chello.at>
2021-08-16 15:23:19 +0200

    Form1.Designer.cs #1

--- src/SQLCompare/Form1.Designer.cs
+++ src/SQLCompare/Form1.Designer.cs
@@ -323,7 +323,7 @@ namespace SQLCompare
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(373, 60);
+            this.textBox1.Size = new System.Drawing.Size(373, 276);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
