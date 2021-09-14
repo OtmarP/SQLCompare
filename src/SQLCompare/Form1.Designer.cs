304ec98ec658f4718b63b4065706da6a16912dd2
Otmar Pilgerstorfer <Otmar.Pilgerstorfer@chello.at>
2021-09-14 07:34:37 +0200

    Form1.Designer.cs #1

--- src/SQLCompare/Form1.Designer.cs
+++ src/SQLCompare/Form1.Designer.cs
@@ -326,7 +326,7 @@ namespace SQLCompare
             this.statusStrip1.Location = new System.Drawing.Point(0, 477);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(915, 22);
-            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.TabIndex = 6;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabelAction
